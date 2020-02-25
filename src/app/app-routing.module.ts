3f5a3157fa539f05175740f7fae827cc77aefa7e
varun1409 <varun.chandra14@gmail.com>
2020-02-24 22:58:08 -0500

    Implement routing  (#2)
    * add header footer; add material icon support, add bootstrap
    
    * create add-media, media-list component, add routing
    
    * create add-media, media-list component, add routing
    
    Co-authored-by: niharika13pjr <niharika13pjr@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AddMediaComponent } from './components/add-media/add-media.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'add-media',
+    component: AddMediaComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

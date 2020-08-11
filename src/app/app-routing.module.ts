4f624a95001dfeba140bd347301f2e1c78c62cbc
Akash1113 <pujariakash@gmail.com>
2020-08-11 18:31:17 -0400

    add media wip

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { MediaListComponent } from './components/media-list/media-list.component';
+
 import { AddMediaComponent } from './components/add-media/add-media.component';
 
 const routes: Routes = [
   {
     path: 'add-media',
     component: AddMediaComponent
+  },
+  {
+    path: '',
+    component: MediaListComponent
+  },
+  {
+    path: 'add-media/back',
+    component: MediaListComponent
   }
 ];
 
